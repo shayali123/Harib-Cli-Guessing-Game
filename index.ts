67b9c253472a436b936a81e55ec77c0eb65c11a5
shayali123 <160710648+shayali123@users.noreply.github.com>
2024-03-24 11:52:46 +0500

    Push Mt Calculator To Github

--- index.ts
+++ index.ts
@@ -3,9 +3,9 @@ import inquirer from "inquirer"
 let random = Math.floor(Math.random() * 10 + 1);
 const answer = await inquirer.prompt([
     {
-        name: "userguessednumber",
-        type: "number",
         message: "Please guess a number 1-10 :",
+        type: "number",
+        name: "userguessednumber",
     },
 ]);
 
